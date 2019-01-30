b273943be2ae8e1e6870b4ff7f598d4ca9875d6e
rashidmajeed <rashid.caliber@gmail.com>
2019-01-30 15:29:14 +0100

    adding header and footer also setting env variables to server file

--- server/app.js
+++ server/app.js
@@ -2,8 +2,14 @@ const express = require("express");
 const routes = require("./routes");
 const path = require("path");
 
+// import configurations
+const configs = require("./config");
+
 // create the new server
 const app = express();
+
+// filtering the enviornment
+const config = configs[app.get("env")];
 
 // enabling Pug Template engine
 app.set("view engine", "pug");
@@ -14,8 +20,18 @@ app.set("views", path.join(__dirname, "./views"));
 // loading the public assets
 app.use(express.static("public"));
 
+// get the current date
+app.use((req, res, next) => {
+  const date = new Date();
+  res.locals.currentYear = date.getFullYear();
+  return next();
+});
+
+// passing sitename to the views
+app.locals.sitetitle = config.sitename;
+
 // listen for the home page
 app.use("/", routes());
 
 // run the application
-app.listen(9000);
+app.listen(8000);
